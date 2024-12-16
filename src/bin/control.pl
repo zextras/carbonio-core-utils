d8d74ea9b9192fca54c0f3cf0e0005b20ead9c60
Gianluca Boiano <morf3089@gmail.com>
2024-12-16 15:54:41 +0100

    chore: control.pl: use absolute path for carbonio call

--- src/bin/control.pl
+++ src/bin/control.pl
@@ -595,7 +595,7 @@ sub getHostName {
 sub displayVersion {
     my $additional_zal_version = "";
     if ( -e $zal_path and -e $zextras_path ) {
-        $additional_zal_version = `carbonio core getVersion`;
+        $additional_zal_version = `/opt/zextras/bin/carbonio core getVersion`;
         chomp $additional_zal_version;
     }
 
