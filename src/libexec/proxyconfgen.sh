d0f629fd70c053be598bf7a0fbe2e7a475fb7030
Davide Frison <davide.frison@zextras.com>
2025-11-17 14:50:02 +0100

    refactor: redefine proxyconfgen.sh jar location
    - using proxyconfgen code moved to carbonio-proxy

--- src/libexec/proxyconfgen.sh
+++ src/libexec/proxyconfgen.sh
@@ -6,4 +6,21 @@
 # SPDX-License-Identifier: GPL-2.0-only
 #
 
-exec /opt/zextras/bin/zmjava com.zimbra.cs.util.proxyconfgen.ProxyConfGen "$@"
+source /opt/zextras/bin/zmshutil || exit 1
+zmsetvars -f
+
+java_options="-XX:ErrorFile=/opt/zextras/log"
+
+if [ "${zimbra_zmjava_java_library_path}" = "" ]; then
+  zimbra_zmjava_java_library_path=/opt/zextras/lib
+fi
+
+# shellcheck disable=SC2086
+exec "${zimbra_java_home}/bin/java" ${java_options} \
+  -client ${zimbra_zmjava_options} \
+  -Dzimbra.home=/opt/zextras \
+  -Djava.library.path=${zimbra_zmjava_java_library_path} \
+  -classpath "/opt/zextras/proxyconfgen/proxyconfgen.jar" \
+  com.zimbra.cs.util.proxyconfgen.ProxyConfGen
+  "$@"
+
