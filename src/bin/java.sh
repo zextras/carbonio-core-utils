f53e09be860029795e4b61b327513f69794d7bbd
Davide Frison <davide.frison@zextras.com>
2025-11-17 17:06:40 +0100

    chore: cleanup zmjava pt.2
    - none of the other directories are used
    - the external directory does not take into account carbonio also, so no need for "external jars"

--- src/bin/java.sh
+++ src/bin/java.sh
@@ -9,14 +9,6 @@
 source /opt/zextras/bin/zmshutil || exit 1
 zmsetvars -f
 
-if [ -d "${zimbra_java_home}/jre" ]; then
-  JRE_EXT_DIR=${zimbra_java_home}/jre/lib/ext
-else
-  JRE_EXT_DIR=${zimbra_java_home}/lib/ext
-fi
-
-ZIMBRA_EXTENSIONS="backup"
-ZIMBRA_EXT_DIR="/opt/zextras/lib/ext-common/*"
 for i in $ZIMBRA_EXTENSIONS; do
   if [ -d "/opt/zextras/lib/ext/$i" ]; then
     ZIMBRA_EXT_DIR="${ZIMBRA_EXT_DIR}:/opt/zextras/lib/ext/$i/*"
@@ -29,18 +21,10 @@ if [ "${zimbra_zmjava_java_library_path}" = "" ]; then
   zimbra_zmjava_java_library_path=/opt/zextras/lib
 fi
 
-if [ "${zimbra_zmjava_java_ext_dirs}" = "" ]; then
-  zimbra_zmjava_java_ext_dirs=${JRE_EXT_DIR}:/opt/zextras/mailbox/jars:${ZIMBRA_EXT_DIR}
-fi
-
-if [ -n "${EXT_JAR_PATH}" ]; then
-  zimbra_zmjava_java_ext_dirs=${zimbra_zmjava_java_ext_dirs}:${EXT_JAR_PATH}
-fi
-
 # shellcheck disable=SC2086
 exec "${zimbra_java_home}/bin/java" ${java_options} \
   -client ${zimbra_zmjava_options} \
   -Dzimbra.home=/opt/zextras \
   -Djava.library.path=${zimbra_zmjava_java_library_path} \
-  -classpath "${zimbra_zmjava_java_ext_dirs}:/opt/zextras/mailbox/jars/*:/opt/zextras/conf" \
+  -classpath "/opt/zextras/mailbox/jars/*" \
   "$@"
