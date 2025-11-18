976b45f3562e7b25a77c746074710c325a34ebac
Davide Frison <davide.frison@zextras.com>
2025-11-18 12:47:45 +0100

    chore: use zmjava to run proxyconfgen.sh

--- src/libexec/proxyconfgen.sh
+++ src/libexec/proxyconfgen.sh
@@ -6,21 +6,5 @@
 # SPDX-License-Identifier: GPL-2.0-only
 #
 
-source /opt/zextras/bin/zmshutil || exit 1
-zmsetvars -f
-
-java_options="-XX:ErrorFile=/opt/zextras/log"
-
-if [ "${zimbra_zmjava_java_library_path}" = "" ]; then
-  zimbra_zmjava_java_library_path=/opt/zextras/lib
-fi
-
-# shellcheck disable=SC2086
-exec "${zimbra_java_home}/bin/java" ${java_options} \
-  -client ${zimbra_zmjava_options} \
-  -Dzimbra.home=/opt/zextras \
-  -Djava.library.path=${zimbra_zmjava_java_library_path} \
-  -classpath "/opt/zextras/lib/jars/proxyconfgen.jar" \
-  com.zimbra.cs.util.proxyconfgen.ProxyConfGen
-  "$@"
+zmjava -classpath "/opt/zextras/lib/jars/proxyconfgen.jar" com.zimbra.cs.util.proxyconfgen.ProxyConfGen "$@"
 
