e90fea8dce6ecabc5bd37f2c6bd9fa6cbb872ec3
Davide Frison <36330277+frisonisland@users.noreply.github.com>
2025-11-18 12:43:55 +0100

    chore: [CO-2869] cleanup unused classpath directories (#125)
    * chore: cleanup zmjava
    
    - all those extensions have been moved inside mailbox, plus some of them are not even defined anymore (twofactor)
    
    * chore: cleanup zmjava pt.2
    
    - none of the other directories are used
    - the external directory does not take into account carbonio also, so no need for "external jars"
    
    * chore: cleanup zmjava pt.3
    
    - remove ext path
    
    * chore: cleanup systemd-envscript.sh
    
    - remove references to ext

--- src/bin/java.sh
+++ src/bin/java.sh
@@ -9,32 +9,10 @@
 source /opt/zextras/bin/zmshutil || exit 1
 zmsetvars -f
 
-if [ -d "${zimbra_java_home}/jre" ]; then
-  JRE_EXT_DIR=${zimbra_java_home}/jre/lib/ext
-else
-  JRE_EXT_DIR=${zimbra_java_home}/lib/ext
-fi
-
-ZIMBRA_EXTENSIONS="backup clamscanner network zimbrahsm zimbrasync twofactorauth"
-ZIMBRA_EXT_DIR="/opt/zextras/lib/ext-common/*"
-for i in $ZIMBRA_EXTENSIONS; do
-  if [ -d "/opt/zextras/lib/ext/$i" ]; then
-    ZIMBRA_EXT_DIR="${ZIMBRA_EXT_DIR}:/opt/zextras/lib/ext/$i/*"
-  fi
-done
-
 java_options="-XX:ErrorFile=/opt/zextras/log"
 
 if [ "${zimbra_zmjava_java_library_path}" = "" ]; then
   zimbra_zmjava_java_library_path=/opt/zextras/lib
-fi
-
-if [ "${zimbra_zmjava_java_ext_dirs}" = "" ]; then
-  zimbra_zmjava_java_ext_dirs=${JRE_EXT_DIR}:/opt/zextras/mailbox/jars:${ZIMBRA_EXT_DIR}
-fi
-
-if [ -n "${EXT_JAR_PATH}" ]; then
-  zimbra_zmjava_java_ext_dirs=${zimbra_zmjava_java_ext_dirs}:${EXT_JAR_PATH}
 fi
 
 # shellcheck disable=SC2086
@@ -42,5 +20,5 @@ exec "${zimbra_java_home}/bin/java" ${java_options} \
   -client ${zimbra_zmjava_options} \
   -Dzimbra.home=/opt/zextras \
   -Djava.library.path=${zimbra_zmjava_java_library_path} \
-  -classpath "${zimbra_zmjava_java_ext_dirs}:/opt/zextras/lib/jars/*:/opt/zextras/mailbox/jars/*:/opt/zextras/conf" \
+  -classpath "/opt/zextras/mailbox/jars/*" \
   "$@"
